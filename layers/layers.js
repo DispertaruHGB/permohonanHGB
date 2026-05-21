f65992e4728f05ba17f5f2d92fca6e4754067f7e
DispertaruHGB <kekancinganhgb@gmail.com>
2026-05-21 10:49:35 +0700

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,46 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleSatelliteHybrid_0 = new ol.layer.Tile({
+            'title': 'Google Satellite Hybrid',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'
+            })
+        });
+var format_HGB_2020_2025_1 = new ol.format.GeoJSON();
+var features_HGB_2020_2025_1 = format_HGB_2020_2025_1.readFeatures(json_HGB_2020_2025_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_HGB_2020_2025_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_HGB_2020_2025_1.addFeatures(features_HGB_2020_2025_1);
+var lyr_HGB_2020_2025_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_HGB_2020_2025_1, 
+                style: style_HGB_2020_2025_1,
+                popuplayertitle: 'HGB_2020_2025',
+                interactive: true,
+    title: 'HGB_2020_2025<br />\
+    <img src="styles/legend/HGB_2020_2025_1_0.png" /> Belum Jelas<br />\
+    <img src="styles/legend/HGB_2020_2025_1_1.png" /> Berkas Dikembalikan<br />\
+    <img src="styles/legend/HGB_2020_2025_1_2.png" /> Bukan SG<br />\
+    <img src="styles/legend/HGB_2020_2025_1_3.png" /> Dikembalikan<br />\
+    <img src="styles/legend/HGB_2020_2025_1_4.png" /> PAG<br />\
+    <img src="styles/legend/HGB_2020_2025_1_5.png" /> Pengukuran Ulang<br />\
+    <img src="styles/legend/HGB_2020_2025_1_6.png" /> Permohonan Dicabut<br />\
+    <img src="styles/legend/HGB_2020_2025_1_7.png" /> PERMOHONAN DICABUT<br />\
+    <img src="styles/legend/HGB_2020_2025_1_8.png" /> SG<br />\
+    <img src="styles/legend/HGB_2020_2025_1_9.png" /> <br />' });
+
+lyr_GoogleSatelliteHybrid_0.setVisible(true);lyr_HGB_2020_2025_1.setVisible(true);
+var layersList = [lyr_GoogleSatelliteHybrid_0,lyr_HGB_2020_2025_1];
+lyr_HGB_2020_2025_1.set('fieldAliases', {'Id': 'Id', 'No. Arsip': 'No. Arsip', 'No. Urut': 'No. Urut', 'No. Surat': 'No. Surat', 'Tgl. Surat': 'Tgl. Surat', 'Pemohon': 'Pemohon', 'Uraian': 'Uraian', 'Jenis Hak': 'Jenis Hak', 'Nomor Hak': 'Nomor Hak', 'Kel/Kal': 'Kel/Kal', 'Kem/Kap': 'Kem/Kap', 'Kota/Kab': 'Kota/Kab', 'Berakhir H': 'Berakhir H', 'Dasar SK': 'Dasar SK', 'Penunjuk': 'Penunjuk', 'Tgl. Rapat': 'Tgl. Rapat', 'Asal-Usul': 'Asal-Usul', 'Kelompok': 'Kelompok', 'Tindak Lan': 'Tindak Lan', 'Plotting': 'Plotting', 'Tahun': 'Tahun', 'Status': 'Status', 'Tgl. Tangg': 'Tgl. Tangg', 'layer': 'layer', 'path': 'path', 'Tgl. Terim': 'Tgl. Terim', 'No. Telp': 'No. Telp', 'Jenis': 'Jenis', 'Progre Sur': 'Progre Sur', 'Status Sur': 'Status Sur', 'Penerima': 'Penerima', 'Tgl. Ambil': 'Tgl. Ambil', 'Koordinat': 'Koordinat', 'Progres Su': 'Progres Su', 'Field32': 'Field32', });
+lyr_HGB_2020_2025_1.set('fieldImages', {'Id': 'TextEdit', 'No. Arsip': 'TextEdit', 'No. Urut': 'TextEdit', 'No. Surat': 'TextEdit', 'Tgl. Surat': 'TextEdit', 'Pemohon': 'TextEdit', 'Uraian': 'TextEdit', 'Jenis Hak': 'TextEdit', 'Nomor Hak': 'TextEdit', 'Kel/Kal': 'TextEdit', 'Kem/Kap': 'TextEdit', 'Kota/Kab': 'TextEdit', 'Berakhir H': 'TextEdit', 'Dasar SK': 'TextEdit', 'Penunjuk': 'TextEdit', 'Tgl. Rapat': 'TextEdit', 'Asal-Usul': 'TextEdit', 'Kelompok': 'TextEdit', 'Tindak Lan': 'TextEdit', 'Plotting': 'TextEdit', 'Tahun': 'TextEdit', 'Status': 'TextEdit', 'Tgl. Tangg': 'TextEdit', 'layer': 'TextEdit', 'path': 'TextEdit', 'Tgl. Terim': 'TextEdit', 'No. Telp': 'TextEdit', 'Jenis': 'TextEdit', 'Progre Sur': 'TextEdit', 'Status Sur': 'TextEdit', 'Penerima': 'TextEdit', 'Tgl. Ambil': 'TextEdit', 'Koordinat': 'TextEdit', 'Progres Su': 'TextEdit', 'Field32': 'TextEdit', });
+lyr_HGB_2020_2025_1.set('fieldLabels', {'Id': 'hidden field', 'No. Arsip': 'hidden field', 'No. Urut': 'hidden field', 'No. Surat': 'hidden field', 'Tgl. Surat': 'hidden field', 'Pemohon': 'inline label - visible with data', 'Uraian': 'hidden field', 'Jenis Hak': 'inline label - always visible', 'Nomor Hak': 'inline label - always visible', 'Kel/Kal': 'inline label - always visible', 'Kem/Kap': 'inline label - always visible', 'Kota/Kab': 'inline label - always visible', 'Berakhir H': 'hidden field', 'Dasar SK': 'hidden field', 'Penunjuk': 'hidden field', 'Tgl. Rapat': 'inline label - always visible', 'Asal-Usul': 'inline label - always visible', 'Kelompok': 'inline label - always visible', 'Tindak Lan': 'hidden field', 'Plotting': 'hidden field', 'Tahun': 'inline label - always visible', 'Status': 'inline label - always visible', 'Tgl. Tangg': 'hidden field', 'layer': 'hidden field', 'path': 'hidden field', 'Tgl. Terim': 'hidden field', 'No. Telp': 'hidden field', 'Jenis': 'hidden field', 'Progre Sur': 'hidden field', 'Status Sur': 'hidden field', 'Penerima': 'inline label - visible with data', 'Tgl. Ambil': 'hidden field', 'Koordinat': 'hidden field', 'Progres Su': 'hidden field', 'Field32': 'hidden field', });
+lyr_HGB_2020_2025_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/HGB_2020_2025_1_style.js
@@ -0,0 +1,113 @@
+var size = 0;
+var placement = 'point';
+function categories_HGB_2020_2025_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'Belum Jelas':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,255,4,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Berkas Dikembalikan':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(0,0,0,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Bukan SG':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(51,232,23,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Dikembalikan':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(0,0,0,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'PAG':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(164,0,234,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Pengukuran Ulang':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,255,4,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Permohonan Dicabut':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(0,0,0,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'PERMOHONAN DICABUT':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(0,0,0,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'SG':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,0,0,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(0,0,0,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_HGB_2020_2025_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("Status");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_HGB_2020_2025_1(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
